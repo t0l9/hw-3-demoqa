5c3bec3c8c3e3fa97af72c1dcd7388a448ebba9d
Kolyshkin A <t0l4ik.kas1@gmail.com>
2025-04-11 18:39:26 +0500

    fix

--- src/test/java/tests/AutomationPracticeForm.java
+++ src/test/java/tests/AutomationPracticeForm.java
@@ -18,8 +18,6 @@ public class AutomationPracticeForm {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        //Configuration.holdBrowserOpen = true;
-
     }
 
 
