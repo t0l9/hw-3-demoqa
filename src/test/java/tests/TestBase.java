e62e8afc9098c116f2b3c5b71b68a83160ffe761
Kolyshkin A <t0l4ik.kas1@gmail.com>
2025-05-20 16:18:40 +0500

    add

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -12,6 +12,8 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("allure", new AllureSelenide());
+
     }
 }
