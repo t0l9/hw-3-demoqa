089f644689aec988d9a061e14f5a3b85724c2d8b
Kolyshkin A <t0l4ik.kas1@gmail.com>
2025-05-21 09:36:17 +0500

    add

--- src/test/java/tests/JavaFakerTest.java
+++ src/test/java/tests/JavaFakerTest.java
@@ -1,20 +1,26 @@
 package tests;
 
-
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
 import static tests.TestData.*;
 
-@Tag("form")
+
 public class JavaFakerTest extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
 
     @Test
+    @Feature("Проверка формы регистрации")
+    @Story("Форма регистрации")
+    @Owner("Kolyshkin A.S.")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "test", url = "https://test.url")
+    @DisplayName("Прохождение формы регистрации с проверкой введенных данных")
+    @Tag("form")
     void fillFormTest(){
 
         registrationPage.openPage()
@@ -43,7 +49,6 @@ public class JavaFakerTest extends TestBase {
                 .checkResult("Subjects", favoriteSubject)
                 .checkResult("Hobbies", favoriteHobby)
                 .checkResult("Address", userAddress).checkResult("State and City", userState + " " + userCity);
-
 
     }
 }
