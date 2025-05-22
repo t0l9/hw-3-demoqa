cbe062e0b54cee787de2722fc7c28d7cd3e5b308
Kolyshkin A <t0l4ik.kas1@gmail.com>
2025-05-22 15:34:44 +0500

    add

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -21,6 +21,7 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+
         Configuration.remote = "https://" + SELENOID_LOG + ":" + SELENOID_PASS + "@" + SELENOID_URL + "/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
